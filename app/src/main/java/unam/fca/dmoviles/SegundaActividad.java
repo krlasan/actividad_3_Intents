48b639efad39aa7f62dfaf8b3b31ad0555f74381
Karla S <karla@iMac-968.local>
2018-09-21 22:27:34 -0500

    Initial commit

---
+++ app/src/main/java/unam/fca/dmoviles/SegundaActividad.java
@@ -0,0 +1,33 @@
+package unam.fca.dmoviles;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+
+
+
+
+public class SegundaActividad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda_actividad);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        
+        TextView tv = findViewById(R.id.tv);
+        tv.setText(message);
+
+
+    }
+
+}
